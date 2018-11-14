ecd58937e689dc4f40aa6a4e3b677e1dbed0f632
sokhyyin <sokhyyin@gmail.com>
2018-11-14 16:27:01 -0700

    Add new plugin

--- wp-content/themes/vantage.1.7.7/header.php
+++ wp-content/themes/vantage.1.7.7/header.php
@@ -19,11 +19,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=10" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--  -->
-	
 
-	
-<!-- 	 -->
 	<?php wp_head(); ?>
 </head>
 
@@ -32,12 +28,9 @@
 <?php do_action('vantage_before_page_wrapper') ?>
 
 <div id="page-wrapper">
-
 	<?php do_action( 'vantage_before_masthead' ); ?>
-
 	<?php if( ! siteorigin_page_setting( 'hide_masthead', false ) ) : ?>
-
-		<?php get_template_part( 'parts/masthead', apply_filters( 'vantage_masthead_type', siteorigin_setting( 'layout_masthead' ) ) ); ?>
+	<?php get_template_part( 'parts/masthead', apply_filters( 'vantage_masthead_type', siteorigin_setting( 'layout_masthead' ) ) ); ?>
 
 	<?php endif; ?>
 
